455975896b657d633e340b258bb49cd6fc7df9d0
temhalore <temha.lore@gmal.com>
2025-02-10 22:38:58 +0300

    BU İYİ AMA DENEMEDİM ŞİMDİ APP SETTİNGSLİ YAPMAYA ÇALIŞACAĞIMBU ÖNCEKİ HAL

---
+++ Temha.DataOkuConsole/DTO/AppSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temha.DataOkuConsole.DTO
+{
+    public class AppSettings
+    {
+        public string FirmaKod { get; set; }
+        public string KaynakDosyaYolu { get; set; }
+        public string HataliDosyaYolu { get; set; }
+        public string LogDosyaYolu { get; set; }
+        public bool IsDebugMode { get; set; }
+        public int MaxRetryCount { get; set; }
+    }
+}
