7daa156ab3d9cd3d9e63b0dc26addb3d7179f145
temhalore <temha.lore@gmal.com>
2025-02-22 00:22:18 +0300

    downloader halde

---
+++ Temha.DataOku.SetupDownloader/indir.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Temha.DataOku.SetupDownloader
+{
+    partial class indir
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            progressBar = new ProgressBar();
+            lblStatus = new Label();
+            btn_download = new Button();
+            SuspendLayout();
+            // 
+            // progressBar
+            // 
+            progressBar.Location = new Point(195, 225);
+            progressBar.Name = "progressBar";
+            progressBar.Size = new Size(411, 29);
+            progressBar.TabIndex = 0;
+            // 
+            // lblStatus
+            // 
+            lblStatus.AutoSize = true;
+            lblStatus.Location = new Point(346, 266);
+            lblStatus.Name = "lblStatus";
+            lblStatus.Size = new Size(100, 15);
+            lblStatus.TabIndex = 1;
+            lblStatus.Text = "indirme durumu..";
+            // 
+            // btn_download
+            // 
+            btn_download.Location = new Point(346, 158);
+            btn_download.Name = "btn_download";
+            btn_download.Size = new Size(75, 23);
+            btn_download.TabIndex = 2;
+            btn_download.Text = "Başla";
+            btn_download.UseVisualStyleBackColor = true;
+            btn_download.Click += button1_Click;
+            // 
+            // indir
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btn_download);
+            Controls.Add(lblStatus);
+            Controls.Add(progressBar);
+            Name = "indir";
+            Text = "indir";
+            Load += indir_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ProgressBar progressBar;
+        private Label lblStatus;
+        private Button btn_download;
+    }
+}
---
+++ Temha.DataOku.SetupDownloader/indir.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Temha.DataOku.SetupDownloader
+{
+    public partial class indir : Form
+    {
+        // Asıl setup dosyasının URL'sini buraya girin.
+        private const string SetupUrl = "https://example.com/your-setup-file.exe";
+        // İndirilecek setup dosyasının yerel adını belirleyin.
+        private const string LocalSetupFileName = "setup.exe";
+
+        public indir()
+        {
+            InitializeComponent();
+        }
+
+        private void indir_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DownloadSetupFile()
+        {
+            using (var webClient = new WebClient())
+            {
+                webClient.DownloadProgressChanged += WebClient_DownloadProgressChanged;
+                webClient.DownloadFileCompleted += WebClient_DownloadFileCompleted;
+
+                try
+                {
+                    // Asenkron indirmeyi başlat.
+                    webClient.DownloadFileAsync(new Uri(SetupUrl), LocalSetupFileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"İndirme sırasında bir hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void WebClient_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            // ProgressBar ve label’ı güncelle.
+            progressBar.Value = e.ProgressPercentage;
+            lblStatus.Text = $"İndiriliyor: {e.ProgressPercentage}%";
+        }
+
+        private void WebClient_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show($"İndirme tamamlanamadı: {e.Error.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (e.Cancelled)
+            {
+                MessageBox.Show("İndirme işlemi iptal edildi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            lblStatus.Text = "İndirme tamamlandı. Kurulum başlatılıyor...";
+            try
+            {
+                // İndirilen dosyayı çalıştır.
+                Process.Start(LocalSetupFileName);
+                Application.Exit(); // Bootstrapper uygulamasını kapat.
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Kurulum başlatılamadı: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // İndirme işlemini başlat.
+            DownloadSetupFile();
+        }
+    }
+}
